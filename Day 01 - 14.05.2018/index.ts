d4a6211d2d8c5e8a69b84138cb3fa4c88a85d3ef
Anna Karp <32166631+AnnaKarpf@users.noreply.github.com>
2018-05-14 18:56:12 +0300

    angular

---
+++ Day 01 - 14.05.2018/index.ts
@@ -0,0 +1,11 @@
+interface Country{
+    name?: number;  //? is for optional
+    fullName: string;
+    capital: string;
+}
+
+
+let obj: Country = {
+    "fullName": "Israel",
+    "capital": "Jerusalem"
+};
